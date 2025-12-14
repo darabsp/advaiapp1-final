d9cd92e17c3611ae7cc23a321b3dc52a17428e84
darabsp <51314762+darabsp@users.noreply.github.com>
2025-12-15 05:15:12 +0900

    [feature] テストコード追加

---
+++ tests/advaiapp1_final/test___init__.py
@@ -0,0 +1,35 @@
+from pytest import MonkeyPatch, fixture
+
+import matplotlib
+from pathlib import Path
+
+from advaiapp1_final import main
+
+
+# NOTE: Tkinterが使えないんだが？というエラーの抑制
+@fixture(autouse=True)
+def make_matplotlib_not_use_gui(
+    monkeypatch: MonkeyPatch,
+):
+    matplotlib.use("Agg")
+    monkeypatch.setattr("matplotlib.pyplot.Figure.show", lambda _: None) # pyright: ignore[reportUnknownLambdaType, reportUnknownArgumentType]
+
+
+class TestMain():
+    @fixture(autouse=True)
+    def patch_for_main(
+        self,
+        monkeypatch: MonkeyPatch,
+        tmp_path: Path,
+    ):
+        # NOTE: main()でwaitforbuttonpressしているのでそのままだとテストが進まない
+        monkeypatch.setattr("matplotlib.pyplot.waitforbuttonpress", lambda: None)
+        monkeypatch.chdir(tmp_path)
+
+    def test_file_output(
+        self,
+        tmp_path: Path,
+    ):
+        main()
+
+        assert tmp_path.joinpath("our_model").with_suffix(".tar").exists()
