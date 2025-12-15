1ec7818b7928bb17c3820d81bda0e4dab48032bd
darabsp <51314762+darabsp@users.noreply.github.com>
2025-12-15 21:20:36 +0900

    [update] 図の表示回数についてもテストする

--- tests/advaiapp1_final/test___init__.py
+++ tests/advaiapp1_final/test___init__.py
@@ -1,35 +1,46 @@
 from pytest import fixture, MonkeyPatch
 
 import matplotlib
+from matplotlib.figure import Figure
 from pathlib import Path
 
 from advaiapp1_final import main
 
 
-# NOTE: Tkinterが使えないんだが？というエラーの抑制
-@fixture(autouse=True)
-def make_matplotlib_not_use_gui(
-    monkeypatch: MonkeyPatch,
-):
-    matplotlib.use("Agg")
-    monkeypatch.setattr("matplotlib.pyplot.Figure.show", lambda _: None) # pyright: ignore[reportUnknownLambdaType, reportUnknownArgumentType]
-
-
 class TestMain():
+    figure_show_count = 0
+
     @fixture(autouse=True)
     def patch_for_main(
         self,
         monkeypatch: MonkeyPatch,
         tmp_path: Path,
     ):
-        # NOTE: main()でwaitforbuttonpressしているのでそのままだとテストが進まない
+        def increment_figure_show_count(_: Figure):
+            self.figure_show_count += 1
+
+        # Tkinterが使えないんだが？？という不具合の対処
+        matplotlib.use("Agg")
+        monkeypatch.setattr("matplotlib.figure.Figure.show", increment_figure_show_count)
+        # main()でwaitforbuttonpressしているのでそのままだとテストが進まない
         monkeypatch.setattr("matplotlib.pyplot.waitforbuttonpress", lambda: None)
+
         monkeypatch.chdir(tmp_path)
+
+        yield
+
+        # 後処理
+        self.figure_show_count = 0
 
     def test_file_output(
         self,
         tmp_path: Path,
     ):
         main()
-
         assert tmp_path.joinpath("out/our_model.tar").exists()
+
+    def test_figure_show_count(
+        self,
+    ):
+        main()
+        assert self.figure_show_count == 2
